3fb2c07214a85ce3fe85461734816a288bf517e7
RavshanSean <ravshansean1995@gmail.com>
2024-07-17 09:59:00 -0500

    done

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,7 +17,13 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+  },
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
     'react/prop-types': 'off', // add this line
-  'react/no-unescaped-entities': 'off' // add this line
+    'react/no-unescaped-entities': 'off' // add this line
   },
 }
